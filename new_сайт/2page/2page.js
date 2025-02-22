c2f0d5118f6278b40c40dceefc12ea7ae5ee4e0e
Codevixx <codevixx.studio@gmail.com>
2025-02-22 14:40:00 +0200

    Delete new_сайт directory

--- new_сайт/2page/2page.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-        document.querySelector(".preloader").style.display = "none";
-        document.querySelector(".content").style.display = "block";
-    }, 2000);
-});
